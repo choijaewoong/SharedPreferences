a6fd71bb5d22e78b6d1517daeb3cc00dedbdba5a
choijaewoong <jwchoi0129@gmail.com>
2015-10-21 12:04:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dongja94/samplesharedpreferences/SettingFragment.java
@@ -0,0 +1,33 @@
+package com.example.dongja94.samplesharedpreferences;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingFragment extends PreferenceFragment {
+
+
+    public SettingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+    //    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        TextView textView = new TextView(getActivity());
+//        textView.setText(R.string.hello_blank_fragment);
+//        return textView;
+//    }
+
+
+}
